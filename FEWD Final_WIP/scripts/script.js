be855815e34319b32dc2e87ecda04f7e6c5c6d02
Alexis Napoliello <42307614+anapoliello@users.noreply.github.com>
2018-10-17 17:42:05 -0400

    10/16 upload of files

---
+++ FEWD Final_WIP/scripts/script.js
@@ -0,0 +1,57 @@
+
+let totalScore = 0;
+
+// Reveal follow-up questions based on your event type choice
+$('#event_type').change(function(followup_questions){
+	let	searchQuestions = this.value + '_questions'
+	$('.active').removeClass('active');
+	$('#' + searchQuestions).addClass('active');
+	totalScore = 0
+});
+
+// Add points to a total score
+
+//need to reevaluate sum functions - currently adding and not adjusting
+
+let displayTotal = function(){
+	console.log(totalScore)
+}
+$('#user_information input').change(function(){
+	totalScore = 0;
+	 $('.active input:checked').each(function() {
+	 		//console.log('totalScore WAS', totalScore)
+	        totalScore += Number($(this).val());
+	       // console.log('totalScore IS', totalScore)
+	    });
+	 displayTotal();
+})
+
+//If yes/no, than questions
+// $('[name="party1"]').change(function(){
+// let answer = $('[name="party1"]:checked').val();
+// 	$('.party1Subquestions').removeClass('active');
+// 	$('.party1Subquestions[value="' + answer + '"]').addClass("active");
+// })
+
+$('.branch [name="branch1"]').change(function(){
+let answer = $('[name="branch1"]:checked').val();
+	$('.branchpart').removeClass('active');
+	$('.branchpart[value="' + answer + '"]').addClass("active");
+})
+
+$('#submitted').click(function(severity){
+	let	severityLevel = this.value + '_advice';
+	let levels = ['low', 'moderate', 'severe'];
+	$('#submitted').addClass('active');
+		if (levels <= 3) {
+			severityLevel = 'low';
+		}
+		else if (levels >= 8) {
+			severityLevel = 'severe';
+		}
+		else {
+			severityLevel = 'moderate';
+		}
+	
+	//totalScore = 0
+});
